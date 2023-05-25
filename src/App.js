93b16ade9a2c6be7339902c15192febd0c727acc
Nika <nikaonime@gmail.com>
2023-05-25 08:50:51 +0300

    setState(board, xPlaying), handleBoxClick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import { Board } from './components/Board';
 
 function App() {
-  const board = Array(9).fill('X')
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true)
+
+  const handleBoxClick = (boxIdx) => {
+    const updateBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying === true ? 'X' : 'O';
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updateBoard)
+    setXPlaying(!xPlaying)
+  }
 
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
     </div>
   );
 }
