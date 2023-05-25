6ca3555a125f9bac6678c7add165df15c69b098b
Nika <nikaonime@gmail.com>
2023-05-25 09:11:10 +0300

    detecting winner(checkWinner, for, win_conditions)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { Board } from './components/Board';
 
 function App() {
 
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ]
+
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true)
 
@@ -16,8 +27,20 @@ function App() {
       }
     })
 
+    checkWinner(updateBoard)
+
     setBoard(updateBoard)
     setXPlaying(!xPlaying)
+  }
+
+  const checkWinner = (board) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        console.log(board[x])
+        return board[x]
+      }
+    }
   }
 
   return (
